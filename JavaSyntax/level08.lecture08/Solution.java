1518c2290285a160ce41ba81b54b7596c9217e5e
Luminescensse <34734436+Luminescensse@users.noreply.github.com>
2018-01-06 12:51:04 +0500

    Ready

---
+++ JavaSyntax/level08.lecture08/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static HashMap<String, String> createMap() {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Иванов", "Иван");
+        map.put("Сидоров", "Петр");
+        map.put("Никитин", "Саша");
+        map.put("Петров", "Дима");
+        map.put("Земин", "Тимур");
+        map.put("Басыров", "Саша");
+        map.put("Хомин", "Руслан");
+        map.put("Лапин", "Федя");
+        map.put("Митин", "Саша");
+        map.put("Семин", "Саша");
+
+        return map;
+    }
+	
+	
+	// Удаляем из Map повторяющиеся значения
+    public static void removeTheFirstNameDuplicates(HashMap<String, String> map) {
+
+        HashMap<String, String> mapcopy = new HashMap<>(map);
+
+        for (HashMap.Entry pair: mapcopy.entrySet()) {
+            if (Collections.frequency(mapcopy.values(), pair.getValue()) > 1) {
+                map.remove(pair.getKey());
+            }
+        }
+
+        System.out.println(map);
+    }
+
+	// удаляем из Map заданное значение
+    public static void removeItemFromMapByValue(HashMap<String, String> map, String value) {
+		
+        HashMap<String, String> copy = new HashMap<String, String>(map);
+		
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getValue().equals(value))
+                map.remove(pair.getKey());
+        }
+    }
+
+    public static void main(String[] args) {
+		
+        removeTheFirstNameDuplicates(createMap());
+    }
+}
